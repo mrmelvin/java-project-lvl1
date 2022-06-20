889c88ecd0706069488bfb148836f14ddca50dcf
mr.melvin <korobov.mike@gmail.com>
2022-06-20 18:37:15 +0500

    Change nethod isPrime in Prime class

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -11,12 +11,16 @@ public class Prime {
             "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
     public static boolean isNumberPrime(int number) {
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                return false;
+        if (number > 1) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
             }
+            return true;
+        } else {
+            return false;
         }
-        return true;
     }
 
     public static void startGame() {
